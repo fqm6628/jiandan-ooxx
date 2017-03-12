4c109dae47564b57b79a1c7846de89df54239e49
fuqumao <f1025520930@gmail.com>
2017-03-12 23:51:01 +0800

    update 20170312  request timeout added to prevent to spider from no response

--- identify-image.py
+++ identify-image.py
@@ -27,7 +27,6 @@ class IdentifyImage():
         return im.size
 
     def getFileList(self, dir, fileList):
-        newDir = dir
         if os.path.isfile(dir):
             fileList.append(dir.decode('gbk'))
         elif os.path.isdir(dir):
@@ -35,8 +34,8 @@ class IdentifyImage():
                 # 如果需要忽略某些文件夹，使用以下代码
                 # if s == "xxx":
                 # continue
-                newDir = os.path.join(dir, s)
-                self.getFileList(newDir, fileList)
+                new_dir = os.path.join(dir, s)
+                self.getFileList(new_dir, fileList)
         return fileList
 
 
